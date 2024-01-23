4bd586650df4a1b5d51c19b1d9c4320b5befebe2
Alona Shelikhova <ashelyaa17@gmail.com>
2024-01-23 19:57:27 +0200

    add

---
+++ http-proxy/server.js
@@ -0,0 +1,89 @@
+'use strict';
+
+
+import * as http from 'node:http';
+import * as net from 'node:net';
+
+const CRLF = '\r\n';
+const PORT = 8000;
+const DEFAULT_HTTP_PORT = 80;
+
+const receiveBody = async (stream) => {
+
+    const chunks = [];
+    for await (const chunk of stream) chunks.push(chunk);
+
+    return Buffer.concat(chunks);
+
+};
+
+const server = http.createServer(async (req, res) => {
+
+    console.log("\nRequest received (HTTP)");
+
+    const { remoteAddress, remotePort } = req.socket;
+
+    console.log(`Connection from ${remoteAddress}:${remotePort} to ${req.url}`);
+    
+    const { headers, url, method } = req;
+    const { pathname, hostname } = new URL(url);
+    const options = { hostname, path: pathname, method, headers };
+    const request = http.request(options, (result) => void result.pipe(res));
+
+    if (method !== 'GET' && method !== 'HEAD') {
+        const body = await receiveBody(req);
+        request.write(body);
+    }
+
+    request.end();
+
+});
+
+server.on('connect', (req, socket, head) => {
+
+    console.log("\nRequest received (HTTPS)");
+
+    socket.write('HTTP/1.1 200 Connection Established' + CRLF + CRLF);
+
+    const { remoteAddress, remotePort } = socket;
+
+    const { hostname, port } = new URL(`http://${req.url}`);
+
+    const targetPort = parseInt(port, 10) || DEFAULT_HTTP_PORT;
+
+    const proxy = net.connect(targetPort, hostname, () => {
+        if (head) proxy.write(head);
+        socket.pipe(proxy).pipe(socket);
+    });
+
+    console.log(`Connection from ${remoteAddress}:${remotePort} to ${hostname}:${targetPort}`);
+
+    proxy.on('error', (err) => {
+
+        console.error(`Proxy connection error: ${err.message}\n`);
+
+        socket.end();
+
+    });
+
+    socket.on('error', (err) => {
+
+        console.error(`Socket error: ${err.message}\n`);
+
+        proxy.end();
+
+    });
+
+    socket.on('end', () => {
+
+        console.log(`Connection from ${remoteAddress}:${remotePort} closed\n`);
+
+        proxy.end();
+
+    });
+
+});
+
+console.log(`Starting HTTP proxy server on port ${PORT}...`)
+
+server.listen(PORT, '0.0.0.0');
